fd7b2acd9fa1df294d03152d1921d964b5b927fd
Jacob Nardella (Swofty) <admin@swofty.net>
2026-09-11 09:55:49 +1000

    Squashed 'apps/judge/judge-server/' content from commit 5ef74c5d
    git-subtree-dir: apps/judge/judge-server
    git-subtree-split: 5ef74c5d6cad9efb2e86a5bb8ff2c90aaa6e435c

---
+++ dmoj/contrib/base.py
@@ -0,0 +1,40 @@
+from typing import TYPE_CHECKING, Union
+
+from dmoj.executors.base_executor import BaseExecutor
+
+if TYPE_CHECKING:
+    from dmoj.cptbox import TracedPopen
+    from dmoj.result import CheckerResult, Result
+
+
+class BaseContribModule:
+    name: str
+    AC = 0
+    WA = 1
+
+    @classmethod
+    def get_checker_args_format_string(cls) -> str:
+        raise NotImplementedError
+
+    @classmethod
+    def get_interactor_args_format_string(cls) -> str:
+        raise NotImplementedError
+
+    @classmethod
+    def get_validator_args_format_string(cls) -> str:
+        raise NotImplementedError
+
+    @classmethod
+    def parse_return_code(
+        cls,
+        proc: 'TracedPopen',
+        executor: BaseExecutor,
+        point_value: float,
+        time_limit: float,
+        memory_limit: int,
+        feedback: str,
+        name: str,
+        stderr: bytes,
+        show_feedback: bool = True,
+    ) -> Union['CheckerResult', 'Result', bool, None]:
+        raise NotImplementedError
